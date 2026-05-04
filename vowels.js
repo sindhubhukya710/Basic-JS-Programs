14da66ec5c8024be718d740732cc686771824d0a
SINDHU <sindhubhukya710@gmail.com>
2026-05-04 20:31:02 +0530

    vowels

---
+++ vowels.js
@@ -0,0 +1,17 @@
+const countVowels = function(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === 'a' || str[i] === 'e' || str[i] === 'i' ||
+      str[i] === 'o' || str[i] === 'u' ||
+      str[i] === 'A' || str[i] === 'E' || str[i] === 'I' ||
+      str[i] === 'O' || str[i] === 'U'
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+};
+console.log(countVowels("sindhu"));
