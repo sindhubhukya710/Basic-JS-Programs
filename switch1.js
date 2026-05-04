926df5aa757e3143c8545c91c1b43613ad9f35c6
SINDHU <sindhubhukya710@gmail.com>
2026-05-04 14:46:45 +0530

    switch1.js

---
+++ switch1.js
@@ -0,0 +1,13 @@
+let a=7
+let b=8
+let operator = "-";
+switch (operator) {
+    case "+":
+        console.log(a+b);
+        break;
+    case "*":
+        console.log(a*b);
+        break;
+    case "-":
+        console.log(a-b);
+}
